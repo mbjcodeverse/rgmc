f83cbff899ae309092558eb3635901888786965a
mbjcodeverse <markhjolive@gmail.com>
2026-01-18 05:41:52 +0800

    changed database name

--- models/connection.php
+++ models/connection.php
@@ -4,7 +4,10 @@ class Connection{
 	public function connect(){
 		// $link = new PDO("mysql:host=localhost;dbname=rgmc", "root", "");
 
-		$link = new PDO("mysql:host=localhost;dbname=u896983687_rgmc", "u896983687_rgmc", "RGMC_onwards2025");
+		// $link = new PDO("mysql:host=localhost;dbname=u896983687_rgmc", "u896983687_rgmc", "RGMC_onwards2025");
+
+		// rivsongoldplast.com
+		$link = new PDO("mysql:host=localhost;dbname=u604140403_rgmc", "u604140403_rgmc", "RGMC_onwards2025");
 
 		$link -> exec("set names utf8");
 		return $link;
